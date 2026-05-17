dec2c5989d2cf706c556ac7d97a0824f822fb27b
Prashant Rahul <prashantrahul141@protonmail.com>
2026-05-18 03:21:28 +0530

    init
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

---
+++ flake.nix
@@ -0,0 +1,97 @@
+{
+  description = "Rust env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
+
+    crane.url = "github:ipetkov/crane";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    _@{
+      self,
+      nixpkgs,
+      crane,
+      fenix,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+
+        toolchain = pkgs.fenix.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-gh/xTkxKHL4eiRXzWv8KP7vfjSk61Iq48x47BEDFgfk=";
+        };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+        src = ./.;
+
+        commonArgs = { inherit src; };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        package =
+          {
+            craneLib,
+            lib,
+            src,
+          }:
+          craneLib.buildPackage {
+            inherit cargoArtifacts src;
+            doCheck = false;
+          };
+      in
+      {
+
+        packages = {
+          inherit package;
+          default = package;
+          checks = {
+            clippy = craneLib.cargoClippy (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+              }
+            );
+
+            fmt = craneLib.cargoFmt {
+              inherit src;
+            };
+
+            doc = craneLib.cargoDoc (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+              }
+            );
+
+            nextest = craneLib.cargoNextest (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+                partitions = 1;
+                partitionType = "count";
+              }
+            );
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [ toolchain ];
+        };
+
+        formatter = pkgs.nixfmt-tree;
+      }
+    );
+}
